91376f31d21939ebc4c9350228bc99365935d647
Lamarr3314 <lamarrellis6@gmail.com>
2023-01-26 21:19:46 -0500

    changed boot up page to format

--- script.js
+++ script.js
@@ -14,14 +14,16 @@ menuIcon.onclick = function () {
       sidePanel[i].style.opacity = "1";
       sidePanel[i].style.transform = "none";
     }
-    menuIsOn=true;
-  }
-  else{
+    menuIsOn = true;
+  } else {
     for (let i = 0; i < sidePanel.length; i++) {
-        sidePanel[i].style.transition = "all " + (i * 5) / 10 + "s";
-        sidePanel[i].style.opacity = "0";
-        sidePanel[i].style.transform = "translateY(100px)";
-      }
-    menuIsOn=false;
+      sidePanel[i].style.transition = "all " + (i * 5) / 10 + "s";
+      sidePanel[i].style.opacity = "0";
+      sidePanel[i].style.transform = "translateY(100px)";
+    }
+    menuIsOn = false;
   }
 };
+window.addEventListener("load", function () {
+  this.document.getElementById("backHome").click = true;
+});
