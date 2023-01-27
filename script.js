f6fe4d5144cbaf4403b9fe85ca430bfdb8b36716
Lamarr3314 <lamarrellis6@gmail.com>
2023-01-26 20:34:49 -0500

    added toggle for the Menu

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ var typed = new Typed(".auto-type", {
   backSpeed: 150,
   loop: false,
 });
+let menuIsOn = false;
 let menuIcon = document.getElementById("menuIcon");
 let sidePanel = document.querySelectorAll("#sidePanel div");
 menuIcon.onclick = function () {
-  for (let i = 0; i < sidePanel.length; i++) {
-    sidePanel[i].style.transition = "all " + (i * 5) / 10 + "s";
-    sidePanel[i].style.opacity = "1";
-    sidePanel[i].style.transform = "none";
+  if (!menuIsOn) {
+    for (let i = 0; i < sidePanel.length; i++) {
+      sidePanel[i].style.transition = "all " + (i * 5) / 10 + "s";
+      sidePanel[i].style.opacity = "1";
+      sidePanel[i].style.transform = "none";
+    }
+    menuIsOn=true;
+  }
+  else{
+    for (let i = 0; i < sidePanel.length; i++) {
+        sidePanel[i].style.transition = "all " + (i * 5) / 10 + "s";
+        sidePanel[i].style.opacity = "0";
+        sidePanel[i].style.transform = "translateY(100px)";
+      }
+    menuIsOn=false;
   }
 };
